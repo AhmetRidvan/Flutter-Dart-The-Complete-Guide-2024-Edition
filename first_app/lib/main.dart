85bfb4a9894d93cead0669fed389384eaeb9f23e
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2024-06-23 14:24:46 +0300

    wd
    wd

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'package:first_app/styledtext.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/gradient_container.dart';
-
+//2:36 
 //Module Summary bitir.
 
 void main() {
@@ -10,7 +10,7 @@ void main() {
     home: Scaffold(
         body: GradiantContainer(
       colors: const [Colors.pinkAccent, Colors.teal],
-    )),
+    )), 
   ));
 }
 // extends özellikleri işlevleri almak
