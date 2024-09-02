245de6f91c436033abcf939846d63d7554324875
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2024-09-02 16:39:09 +0300

    sd
    sd

--- expense_tracker/lib/widgets/modal.dart
+++ expense_tracker/lib/widgets/modal.dart
@@ -10,13 +10,66 @@ class Modal extends StatefulWidget {
 }
 
 class _Modal extends State<Modal> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(20),
-      child: Column(children: [
-
-      ],),
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text("Title"),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                  },
+                  child: const Text("Save"))
+            ],
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              label: Text("Amount"),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_amountController.text);
+                  },
+                  child: const Text("Save")),
+              ElevatedButton(
+                  onPressed: () {
+                    _amountController.clear();
+                    _titleController.clear();
+                  },
+                  child: const Text("Clear")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel")),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
