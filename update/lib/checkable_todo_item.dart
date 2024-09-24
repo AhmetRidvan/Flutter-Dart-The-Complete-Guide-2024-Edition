3189df184aedfa47e8f9612bd10b645712048923
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2024-09-24 17:35:01 +0300

    wd
    wd

--- update/lib/checkable_todo_item.dart
+++ update/lib/checkable_todo_item.dart
@@ -14,6 +14,7 @@ class CheckableTodoItem extends StatefulWidget {
 
 class _CheckableTodoItemState extends State<CheckableTodoItem> {
   var _done = false;
+  
 
   void _setDone(bool? isChecked) {
     setState(() {
