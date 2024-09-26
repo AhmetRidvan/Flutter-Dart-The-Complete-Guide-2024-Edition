d145a6aad051ed464d116ff0a225a7124ad73d5c
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2024-09-26 17:15:14 +0300

    df
    df

---
+++ Widget, Element, Render tree, key/a.dart
@@ -0,0 +1,6 @@
+/*
+Widget tree tasarım planıdır.
+Element tree Widget tree'yi kopyalar ve durumları kontrol eder.
+Render tree Element tree'ya bakarak çizim yapar.
+Key Widgetler'ı benzersiz şekilde tanımlar. her hangi bir yer de öğe güncellenirse Flutter hangi öğenin güncellendiğini key sayesinde belirler.
+*/
--- update/lib/main.dart
+++ update/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:update/keys.dart';
 
 import 'package:update/old/ui_updates_demo.dart';
-// Hem widget ağacında hem de element ağacında güncellenen kısımlar kirlenir (widgetlerler alt widgetleriyle birlikte) ve yeniden oluşturulur. yeni element tree render tree'de çizilir.
+
+
 void main() {
   runApp(const App());
 }
@@ -22,4 +23,3 @@ class App extends StatelessWidget {
     );
   }
 }
-
