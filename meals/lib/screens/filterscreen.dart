abfdd628ac6b296ad5671e9528cdb74d2474f003
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2024-10-26 20:46:45 +0300

    ef
    ef

--- meals/lib/screens/filterscreen.dart
+++ meals/lib/screens/filterscreen.dart
@@ -16,20 +16,23 @@ class FilterScreen extends StatefulWidget {
 }
 
 class _FilterScreen extends State<StatefulWidget> {
-  bool b1 = false;
+  bool _gluten = false;
+  bool _lactose = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Mydrawer(
-        merhaba: (selam) {
-          Navigator.of(context).pop();
-          if (selam == "m") {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => TabScreen(),
-            ));
-          }
-        },
-      ),
+      // drawer: Mydrawer(
+      //   merhaba: (selam) {
+      //     Navigator.of(context).pop();
+      //     if (selam == "m") {
+      //       Navigator.of(context).pushReplacement(MaterialPageRoute(
+      //         builder: (context) => TabScreen(),
+      //       ));
+      //     }
+      //   },
+      // ),
       appBar: appBar("Your filters !", context),
       body: Column(
         children: [
@@ -49,11 +52,80 @@ class _FilterScreen extends State<StatefulWidget> {
                     .titleLarge!
                     .copyWith(color: Theme.of(context).colorScheme.onSurface),
               ),
-              value: b1,
+              value: _gluten,
               contentPadding: EdgeInsets.only(left: 30, right: 30),
               onChanged: (value) {
                 setState(() {
-                  b1 = value;
+                  _gluten = value;
+                });
+              }),
+              SwitchListTile(
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              subtitle: Text(
+                "Only include lactose-free meals !",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              title: Text(
+                "Lactose-free",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              value: _lactose,
+              contentPadding: EdgeInsets.only(left: 30, right: 30),
+              onChanged: (value) {
+                setState(() {
+                  _lactose = value;
+                });
+              }),
+              SwitchListTile(
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              subtitle: Text(
+                "Only include vegetarian-free meals !",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              title: Text(
+                "vegetarian-free",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              value: _vegetarian,
+              contentPadding: EdgeInsets.only(left: 30, right: 30),
+              onChanged: (value) {
+                setState(() {
+                  _vegetarian = value;
+                });
+              }),
+              SwitchListTile(
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              subtitle: Text(
+                "Only include vegan-free meals !",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              title: Text(
+                "vegan-free",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              value: _vegan,
+              contentPadding: EdgeInsets.only(left: 30, right: 30),
+              onChanged: (value) {
+                setState(() {
+                  _vegan = value;
                 });
               })
         ],
