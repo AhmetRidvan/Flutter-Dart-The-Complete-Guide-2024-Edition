bdf1af080846cb5bd1f95f715d74c732c70e9bde
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2024-12-01 22:33:11 +0300

    bc
    bc

--- Apps/foods/lib/widgets/dra_wer.dart
+++ Apps/foods/lib/widgets/dra_wer.dart
@@ -6,17 +6,65 @@ class Dra_wer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.onPrimary,
+      shadowColor: Colors.lightBlue,
+      width: 350,
+      elevation: 333,
       child: Column(
         children: [
           DrawerHeader(
             decoration: BoxDecoration(
                 gradient: LinearGradient(colors: [
               Theme.of(context).colorScheme.primaryContainer,
-              Theme.of(context).colorScheme.error
+              Theme.of(context).colorScheme.onPrimaryContainer
             ])),
-            child: Text("s"),
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Icon(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  Icons.fastfood,
+                  size: 50,
+                ),
+                const SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  "Cooking Up!",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.onPrimary),
+                )
+              ],
+            ),
           ),
+          ListTile(
+            leading: Icon(
+              Icons.restaurant,
+              size: 26,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            title: Text(
+              "Foods",
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface, fontSize: 30),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.filter_alt,
+              size: 26,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            title: Text(
+              "Filters",
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface, fontSize: 30),
+            ),
+            onTap: () {},
+          )
         ],
       ),
     );
