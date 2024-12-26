cf8ffeded5c1f1511af836b311da51c9a4d51593
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2024-12-26 23:37:11 +0300

    df
    df

--- Apps/foods/lib/main.dart
+++ Apps/foods/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:foods/screens/bottom_navigation_bar_screen.dart';
 import 'package:foods/screens/nothing.dart';
 
-//2:46
+//6:27
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
--- Apps/foods/lib/providers/favorite_food_provider.dart
+++ Apps/foods/lib/providers/favorite_food_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:foods/models/food_model.dart';
 
-class FavoriteFoodProvider extends StateNotifier<List<FoodModel>> {
-  FavoriteFoodProvider(super.key);
+class FavoriteFoodNotifier extends StateNotifier<List<FoodModel>> {
+  FavoriteFoodNotifier() : super([]); //asdsa
+
+  
 }
 
 var favoriteFoodsProvider = StateNotifierProvider();
