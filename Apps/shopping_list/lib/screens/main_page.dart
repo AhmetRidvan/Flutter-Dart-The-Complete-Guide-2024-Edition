cdfbf36cc59e8aae31f1f141f0467964544a1d4e
AhmetRidvan <ahmetridvanordulu@gmail.com>
2025-01-19 11:20:54 +0300

    sd
    sd

--- Apps/shopping_list/lib/screens/main_page.dart
+++ Apps/shopping_list/lib/screens/main_page.dart
@@ -12,32 +12,6 @@ class MainPage extends StatefulWidget {
 class _MainPage extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.onPrimary,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.onPrimary,
-        title: Text("Your Groceries"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: groceryItemsList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Container(
-                    color: groceryItemsList[index].category.color,
-                    width: 30,
-                    height: 30,
-                  ),
-                  title: Text(groceryItemsList[index].name),
-                  trailing: Text(groceryItemsList[index].quantity.toString()),
-                );
-              },
-            ),
-          )
-        ],
-      ),
-    );
+    return ;
   }
 }
---
+++ Apps/shopping_list/lib/widgets/grocery_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GroceryList extends StatelessWidget {
+  const GroceryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Groceries"),
+        
+      ),
+      body: , //2.52
+    );
+  }
+}
