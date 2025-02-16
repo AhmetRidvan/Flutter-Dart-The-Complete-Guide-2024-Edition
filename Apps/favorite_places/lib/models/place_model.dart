ab26507913fcc8eaeb4a8c0f01ffe5a5089f62af
AhmetRidvan <ahmetridvanordulu@gmail.com>
2025-02-17 00:33:37 +0300

    ef
    ef

--- Apps/favorite_places/lib/models/place_model.dart
+++ Apps/favorite_places/lib/models/place_model.dart
@@ -4,10 +4,20 @@ import 'package:uuid/uuid.dart';
 
 final u1 = Uuid();
 
+class PlaceLocation {
+  PlaceLocation(
+      {required this.latitude, required this.longitude, required this.adress});
+  double latitude;
+  double longitude;
+  String adress;
+}
+
 class Placemodel {
-  Placemodel({required this.title,required this.file}) : id = u1.v4(); //otomatik atama
+  Placemodel({required this.title, required this.file,required this.location })
+      : id = u1.v4(); //otomatik atama
 
   String id;
   String title;
   File file;
+  PlaceLocation location;
 }
--- Apps/favorite_places/lib/widgets/location_input.dart
+++ Apps/favorite_places/lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:favorite_places/models/place_model.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
@@ -14,7 +15,18 @@ class LocationInput extends StatefulWidget {
 }
 
 class _locationInput extends State<LocationInput> {
+String key = "AIzaSyCNsqbFhqmjze6fJI_aMVeQNGeIccy9Esk";
   bool _isGettingLocation = false;
+  PlaceLocation? picketLocation;
+
+  String get locationImageUrl {
+    if (picketLocation == null) {
+      return '';
+    }
+    final lat = picketLocation!.latitude;
+    final long = picketLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$long&zoom=20&size=600x300&maptype=roadmap&markers=color:yellow%7Clabel:S%7C$lat,$long&key=$key';
+  }
 
   void getCurrentLocation() async {
     Location location = Location();
@@ -45,16 +57,21 @@ class _locationInput extends State<LocationInput> {
     locationData = await location.getLocation();
     final lat = locationData.latitude;
     final long = locationData.longitude;
-    String _key = "AIzaSyCNsqbFhqmjze6fJI_aMVeQNGeIccy9Esk";
-
-    final _url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=$_key');
-    final _response = await http.get(_url);
-    final _converted = jsonDecode(_response.body);
-    final _data = _converted["results"][0]["formatted_address"];
     
 
+    if (lat == null || long == null) {
+      return;
+    }
+
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=$key');
+    final response = await http.get(url);
+    final converted = jsonDecode(response.body);
+    final adress = converted["results"][0]["formatted_address"];
+
     setState(() {
+      picketLocation =
+          PlaceLocation(latitude: lat, longitude: long, adress: adress);
       _isGettingLocation = false;
     });
 
