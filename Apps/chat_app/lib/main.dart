44c55d1f5a26583785d27112bd740660651caa38
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2025-02-28 16:55:26 +0300

    ed
    ed

--- Apps/chat_app/lib/main.dart
+++ Apps/chat_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/screens/chat_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -12,12 +13,17 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); //4:28
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AuthScreen(), 
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) { //5:50
+          return ;
+        },  
+      ),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
       ),
