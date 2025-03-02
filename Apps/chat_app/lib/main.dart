447299c65c3f2b2e625be0fd86a7c53925df830a
AhmetRidvan <ahmetridvanordulu@gmail.com>
2025-03-02 18:23:55 +0300

    ef
    ef

--- Apps/chat_app/lib/main.dart
+++ Apps/chat_app/lib/main.dart
@@ -1,10 +1,12 @@
-import 'package:chat_app/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:flutter/material.dart';
 
-import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/splash_screen.dart';
+import 'firebase_options.dart';
+import 'package:chat_app/screens/auth_screen.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,17 +14,24 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { //0:36
   const MyApp({super.key});
 
-  @override
+  @override 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) { //5:50
-          return ;
-        },  
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return ChatScreen();
+          } else {
+            return AuthScreen();
+          }
+        },
       ),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
--- Apps/chat_app/lib/screens/chat_screen.dart
+++ Apps/chat_app/lib/screens/chat_screen.dart
@@ -1,12 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-  ChatScreen({super.key});
+  const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Chat App")),
+      appBar: AppBar(
+        title: Text("Chat app"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
       body: Center(child: Text("Logged in!")),
     );
   }
