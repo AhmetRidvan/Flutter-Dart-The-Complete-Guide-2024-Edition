cb5a402203ce1139a43a4fdba02e470069a27492
AhmetRidvan <ahmetridvanordulu@gmail.com>
2025-03-17 01:51:14 +0300

    ef
    ef

--- Apps/chat_app/lib/screens/chat_screen.dart
+++ Apps/chat_app/lib/screens/chat_screen.dart
@@ -1,11 +1,34 @@
-import 'package:chat_app/widgets/chat_messages.dart';
-import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+import 'package:chat_app/widgets/new_message.dart';
+import 'package:chat_app/widgets/chat_messages.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  setupPushNotifications() async {
+    final x = FirebaseMessaging.instance;
+    await x.requestPermission();
+    
+    String? token = await x.getToken();
+    print("Token ==== $token");
+  }
+
+  @override
+  void initState() {
+    // initState'de async işlem yapılmaz flutter istemiyor.
+
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +48,7 @@ class ChatScreen extends StatelessWidget {
         ],
       ),
       body: Column(
-        children: const [Expanded(child: ChatMessages()), NewMessage(),],  
+        children: const [Expanded(child: ChatMessages()), NewMessage()],
       ),
     );
   }
